1759b302631583614627e1147a399a5bebc93349
zaskoh <117531688+zaskoh@users.noreply.github.com>
2022-12-18 18:11:34 +0100

    added requiresAuth, Auth, RolesAuthority and MultiRolesAuthority from solmate

--- src/issues/M/centralizationRisk.ts
+++ src/issues/M/centralizationRisk.ts
@@ -5,7 +5,7 @@ const issue: RegexIssue = {
   type: IssueTypes.M,
   title: 'Centralization Risk for trusted owners',
   impact: 'Contracts have owners with privileged rights to perform admin tasks and need to be trusted to not perform malicious updates or drain funds.',
-  regex: /( onlyOwner )|( onlyRole\()|(Owned)!?([(, ])|(Ownable)!?([(, ])|(Ownable2Step)!?([(, ])|(AccessControl)!?([(, ])|(AccessControlCrossChain)!?([(, ])|(AccessControlEnumerable)!?([(, ])/g,
+  regex: /( onlyOwner )|( onlyRole\()|( requiresAuth )|(Owned)!?([(, ])|(Ownable)!?([(, ])|(Ownable2Step)!?([(, ])|(AccessControl)!?([(, ])|(AccessControlCrossChain)!?([(, ])|(AccessControlEnumerable)!?([(, ])|(Auth)!?([(, ])|(RolesAuthority)!?([(, ])|(MultiRolesAuthority)!?([(, ])/g,
 };
 
 export default issue;
