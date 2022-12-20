220107d82e133d0fbfcb2a4d82c2fed6d7673685
Just Dravee <47150934+JustDravee@users.noreply.github.com>
2022-12-20 16:55:58 +0100

    I'm still seeing this in C4 reports xD

---
+++ src/issues/GAS/payableFunctions.ts
@@ -0,0 +1,12 @@
+import { IssueTypes, RegexIssue } from '../../types';
+
+const issue: RegexIssue = {
+  regexOrAST: 'Regex',
+  type: IssueTypes.GAS,
+  title: 'Functions guaranteed to revert when called by normal users can be marked `payable`',
+  description:
+    'If a function modifier such as `onlyOwner` is used, the function will revert if a normal user tries to pay the function. Marking the function as `payable` will lower the gas cost for legitimate callers because the compiler will not include checks for whether a payment was provided.',
+  regex: /function((?!payable).)*only/g,
+};
+
+export default issue;
