2b98a0f6f0e4abb77b0b9362d6844204d91ae50c
Entreprenerd <alex@entreprenerd.xyz>
2023-01-24 20:05:33 +0100

    feat: frontRunnable description

--- src/issues/L/frontRunnableInitializer.ts
+++ src/issues/L/frontRunnableInitializer.ts
@@ -4,6 +4,7 @@ const issue: RegexIssue = {
   regexOrAST: 'Regex',
   type: IssueTypes.L,
   title: 'Initializers could be front-run',
+  description: "Initializers could be front-run, allowing an attacker to either set their own values, take ownership of the contract, and in the best case forcing a re-deployment",
   regex: /initialize\(|init\(|(initializer)/g,
 };
 
