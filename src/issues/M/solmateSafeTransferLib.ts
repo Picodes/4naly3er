e5d531f4410bc576c82cbcdae7df8a4c599a55d3
zaskoh <117531688+zaskoh@users.noreply.github.com>
2023-02-04 16:17:42 +0100

    formatting

--- src/issues/M/solmateSafeTransferLib.ts
+++ src/issues/M/solmateSafeTransferLib.ts
@@ -4,7 +4,7 @@ const issue: RegexIssue = {
   regexOrAST: 'Regex',
   type: IssueTypes.M,
   title:
-    ' `Solmate\'s SafeTransferLib does not check for token contract\'s existence`',
+    ' Solmate\'s SafeTransferLib does not check for token contract\'s existence',
   description:
     'There is a subtle difference between the implementation of solmate’s SafeTransferLib and OZ’s SafeERC20: OZ’s SafeERC20 checks if the token is a contract or not, solmate’s SafeTransferLib does not.\n'
     + 'https://github.com/transmissions11/solmate/blob/main/src/utils/SafeTransferLib.sol#L9 \n'
