adad4d29613ea396a79d567c4ab5c8b9f80f7938
0xunforgiven <108366834+0xunforgiven@users.noreply.github.com>
2023-11-02 04:06:52 -0700

    fix delegateCall in loops check (#33)
    Co-authored-by: DESKTOP-QGP5SUI <j@fast.ai>

--- src/issues/H/delegateCallInLoop.ts
+++ src/issues/H/delegateCallInLoop.ts
@@ -5,7 +5,14 @@ const issue: RegexIssue = {
   type: IssueTypes.H,
   title: 'Using `delegatecall` inside a loop',
   impact: 'When calling `delegatecall` the same `msg.value` amount will be accredited multiple times.',
-  regex: /for[^\(]?\([^\)]*\)?.\{((.*[^\}])\n)*.*delegatecall/g,
+  regex: /(for|while|do)[^\(]?(\([^\)]*\))?.?\{(([^\}]*\n)*(([^\}]*\{)([^\{\}]*\n)*([^\{\}]*\}[^\}]*)\n))*[^\}]*delegatecall/g,
+         // (for|while|do)            : detects the loop keyword.             "for"
+         // [^\(]?(\([^\)]*\))?.?\{   : detects loop conditions.              "(....) {"
+         // ([^\}]*\n)*               : detects the line without }.           "......\n"
+         // ([^\}]*\{)                : detects start of the line untile {.   ".......{"
+         // ([^\{\}]*\n)*             : detects the lines without { and }.    "......\n"
+         // ([^\{\}]*\}[^\}]*)\n      : detect the line with }.               "...}..\n"
+         // [^\}]*delegatecall        : detect the lien with delegatecall.    "....call"
 };
 
 export default issue;
